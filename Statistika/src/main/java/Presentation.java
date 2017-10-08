b80fc8157a3da1e1cf040ace6f2bfad99a7a2f23
mirosmith <simir@seznam.cz>
2017-10-08 15:47:02 +0200

    added outputFile and outputFileCheck methods

--- Statistika/src/main/java/Presentation.java
+++ Statistika/src/main/java/Presentation.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 
 public class Presentation {
@@ -20,7 +23,7 @@ public class Presentation {
 		
 		Map<String, Integer> map = getMap();
 		
-		System.out.println("------ WORDS STATISTICS ------");
+		System.out.println("------ WORD STATISTICS ------");
 		
 		for (Map.Entry<String, Integer> item : map.entrySet()) {
 			System.out.format("%-15s %d %n", item.getKey(), item.getValue());
@@ -28,6 +31,75 @@ public class Presentation {
 		
 	}
 	
+	private boolean outputFileCheck(String fName) {
+		
+		if (fName == null) {
+			System.out.println("error due processing");
+			return false;
+		}
+		
+		fName = fName.trim();
+		
+		if (!fName.endsWith(".txt")) {
+			System.out.println("file name must end with \".txt\"");
+			return false;
+		}
+		else {
+			String nameBeforeSuffix = fName.substring(0, fName.indexOf(".")).trim();
+			
+			if (nameBeforeSuffix.isEmpty()) {
+				System.out.println("file name must contain letters");
+				return false;
+			}
+			else {				
+				char firstLetter = nameBeforeSuffix.charAt(0);
+				int intFirstLetter = (int) firstLetter;				
+				
+				if ((intFirstLetter < 65 || intFirstLetter > 90) && (intFirstLetter < 97 || intFirstLetter > 122)) {
+					System.out.println("file name must start with a letter");
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
+	public void outputFile(String outputFileName) {
+		
+		if (logic == null) {
+			System.out.println("error due processing");
+		}	
+		
+		if (outputFileCheck(outputFileName)) {
+			
+			BufferedWriter bw = null;
+			try {
+				Map<String, Integer> myMap = getMap();
+				
+				bw = new BufferedWriter(new FileWriter(outputFileName));
+				
+				bw.write("------ WORD STATISTICS ------\n");
+				for (Map.Entry<String, Integer> item : myMap.entrySet()) {
+					bw.write(item.getKey() + "     " + item.getValue() + "\n");					
+				}
+				System.out.println("Successfully saved into " + outputFileName);
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					bw.close();
+				} catch (IOException e) {				
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		
+		
+	}
 	
 
 }
